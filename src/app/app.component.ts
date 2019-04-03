ef1de0d8f9311730cf8aa4df0c9e7f0c76e5f8d7
Mikyla R Wilfred <mikyla.wilfred@gmail.com>
2019-04-02 21:03:34 -0400

    Started making the tour of heroes more clean

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Dungeons and Dragon\'s Character Sheet';
+  title = 'Dungeons & Dragons Characters';
 }
